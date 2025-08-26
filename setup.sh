de60be3e303c33a7fd1b0a48b5d64ff835c78ece
maybeken <ken.lam@grd0.net>
2025-08-26 19:51:35 +1200

    devops: setup script for mountpoint

--- setup.sh
+++ setup.sh
@@ -18,4 +18,12 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt update
-sudo apt install -y htop docker-ce rsync docker-compose-plugin
+sudo apt install -y htop docker-ce rsync docker-compose-plugin cifs-utils autofs
+
+## Setup AutoFS for Nextcloud data mountpoint
+sudo mkdir -p /root/storage/nextcloud
+echo '/root/storage auto.cifs' | sudo tee -a /etc/auto.master
+echo 'nextcloud -fstype=cifs,iocharset=utf8,rw,seal,credentials=/root/storage-cred.txt,uid=82,gid=82,file_mode=0660,dir_mode=0770 ://u487062-sub1.your-storagebox.de/u487062-sub1' | sudo tee -a /etc/auto.cifs
+sudo chmod 644 /etc/auto.cifs
+sudo systemctl enable autofs
+sudo service autofs restart
