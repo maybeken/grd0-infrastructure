d0fcbb32a95f90ffe5df6ac2a86e2075129aca0c
Maybe Ken <ken.lam@grd0.net>
2026-01-26 06:19:09 +0000

    merge Debian 13 upgrade

--- setup.sh
+++ setup.sh
@@ -18,12 +18,12 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt update
-sudo apt install -y htop docker-ce rsync docker-compose-plugin
+sudo apt install -y htop docker-ce rsync docker-compose-plugin cifs-utils autofs
 
-sudo modprobe nfs 
-sudo modprobe nfsd
-sudo modprobe rpcsec_gss_krb5
-
-echo nfs | sudo tee -a /etc/modules
-echo nfsd | sudo tee -a /etc/modules
-echo rpcsec_gss_krb5 | sudo tee -a /etc/modules
+## Setup AutoFS for Nextcloud data mountpoint
+sudo mkdir -p /root/storage/nextcloud
+echo '/root/storage auto.cifs' | sudo tee -a /etc/auto.master
+echo 'nextcloud -fstype=cifs,iocharset=utf8,rw,seal,credentials=/root/storage-cred.txt,uid=82,gid=82,file_mode=0660,dir_mode=0770 ://u487062-sub1.your-storagebox.de/u487062-sub1' | sudo tee -a /etc/auto.cifs
+sudo chmod 644 /etc/auto.cifs
+sudo systemctl enable autofs
+sudo service autofs restart
